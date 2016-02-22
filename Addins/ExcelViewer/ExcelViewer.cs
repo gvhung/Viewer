05b00cd96c7869508968541debe4a4dd5dbbecc0
furesoft <chris.anders@outlook.de>
2016-02-22 14:30:49 +0100

    added excelviewer addin

---
+++ Addins/ExcelViewer/ExcelViewer.cs
@@ -0,0 +1,46 @@
+﻿using Base;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelViewer
+{
+	public class DisplayBinding : IDisplayBinding
+	{
+		public IViewContent OpenFile(string fileName)
+		{
+			string extension = Path.GetExtension(fileName).ToLowerInvariant();
+			switch (extension)
+			{
+				case ".xlsx": return new DocumentViewContent(fileName);
+			}
+
+			return null;
+		}
+	}
+	
+	public class DocumentViewContent : FileViewContent
+	{
+		unvell.ReoGrid.ReoGridControl textBox = new unvell.ReoGrid.ReoGridControl();
+		
+		public DocumentViewContent()
+		{
+			
+		}
+		
+		public DocumentViewContent(string fileName) : this()
+		{
+			Load(fileName);
+		}
+
+		private async void Load(string fileName)
+		{
+			textBox.Readonly = true;
+			textBox.Load(fileName);
+
+			this.FileName = fileName;
+		}
+
+		public override Control Control => textBox;
+	}
+}
